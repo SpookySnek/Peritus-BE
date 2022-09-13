81987a0c307c4b2396728f7e4fc4235b4d4b6c0a
SpookySnek <carleric.olsson@gmail.com>
2022-09-13 16:56:09 +0200

    Finalized docker set-up

---
+++ Peritus-BE/Context/PeritusDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Peritus_BE.Context
+{
+    public class PeritusDbContext : DbContext
+    {
+        public PeritusDbContext(DbContextOptions<PeritusDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- Peritus-BE/Program.cs
+++ Peritus-BE/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Peritus_BE.Context;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PeritusDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
 
 var app = builder.Build();
 
